b81124f0bec57638f9c92f13fc8bba2f1199c7eb
diegok <diego@freekeylabs.com>
2010-03-23 04:52:23 +0100

    Autogenerate i18n tables on the fly

--- lib/DBIx/Class/I18NColumns.pm
+++ lib/DBIx/Class/I18NColumns.pm
@@ -17,6 +17,7 @@ sub add_i18n_columns {
 
     $self->_i18n_columns( {} ) unless defined $self->_i18n_columns();
     $self->resultset_class( 'DBIx::Class::ResultSet::I18NColumns' );
+    $self->_create_i18n_result_source;
 
     # Add columns & accessors
     while ( my $column = shift @columns ) {
@@ -43,33 +44,53 @@ sub add_i18n_columns {
 sub _create_i18n_result_source {
     my $self = shift;
 
-    $self->_create_i18n_result_source;
-    print STDERR $_ for $self->result_source->schema->sources;
-    my $class = ( blessed($self) || $self ) . 'Test';
-    print STDERR $class, "\n";
-    Class::C3::Componentised->inject_base($class, 'DBIx::Class');
+    if ( my $tablename = $self->table ) {
+        my $class = $self->result_class . 'I18N';
+        Class::C3::Componentised->inject_base( $class, 'DBIx::Class' );
 
-    $class->load_components( qw/ ForceUTF8 Core / );
+        $class->load_components(qw/ ForceUTF8 Core /);
+        $class->table( $tablename . '_i18n' );
+        my $fk_name = 'id_' . $tablename; 
+        $class->add_columns(
+            $fk_name,
+            { data_type => 'INT', default_value => 0, is_nullable => 0 },
+            'language',
+            {
+                data_type     => 'VARCHAR',
+                default_value => '',
+                is_nullable   => 0,
+                size          => 2
+            },
+            'attr',
+            { data_type => 'VARCHAR', is_nullable => 0, size => 32 },
+            'varchar',
+            { data_type => 'VARCHAR', is_nullable => 1, size => 255 },
+            'text',
+            { data_type => 'TEXT', is_nullable => 1 },
+        );
 
-    $class->table( 'on_the_fly' );
-    $class->add_columns(
-        'id_item',
-        { data_type => 'INT', default_value => 0, is_nullable => 0 },
-        'language',
-        { data_type => 'VARCHAR', default_value => '', is_nullable => 0, size => 2 },
-        'attr',
-        { data_type => 'VARCHAR', is_nullable => 0, size => 32 },
-        'varchar',
-        { data_type => 'VARCHAR', is_nullable => 1, size => 255 },
-        'text',
-        { data_type => 'TEXT', is_nullable => 1 },
-    );
+        $class->set_primary_key( $fk_name, "language", "attr" );
+        $self->schema_class->register_class(
+            $self->_i18n_class_moniker => $class );
+    }
+    else {
+        $self->throw_exception(
+            "Cannot create table for i18n strings without a table set on "
+              . $self->result_class );
+    }
+}
 
-    $class->set_primary_key("id_item", "language", "attr");
+sub schema_class {
+    my $self = shift;
 
-    #$self->result_source->schema->register_class( ( blessed($self) || $self ) . 'Test' => $class );
-    #$self->result_source->schema->register_source( ( blessed($self) || $self ) . 'Test' => $class );
+    if ( blessed($self) ) {
+        return blessed( $self->result_source->schema );
+    }
 
+    # this is a horrible and fragile hack to find the schema class :(
+    my $schema_class = $self;
+    $schema_class =~ s/::Result.+$//;
+    return $schema_class;
 }
 
 sub _i18n_method {
@@ -99,11 +120,14 @@ sub foreign_column { 'id_' . shift->result_source->name }
 sub i18n_resultset {
     my $self = shift;
 
-    my $i18n_rs_class = blessed( $self ) . 'I18N';
-    print STDERR $i18n_rs_class, "\n";
-    my ($i18n_rs_name) = $i18n_rs_class =~ /([^:]+)$/;
 
-    return $self->result_source->schema->resultset( $i18n_rs_name );
+    return $self->result_source->schema->resultset( $self->_i18n_class_moniker );
+}
+
+sub _i18n_class_moniker {
+    my $self = shift;
+    my $i18n_rs_class = ( blessed( $self ) || $self ) . 'I18N';
+    my ($i18n_rs_name) = $i18n_rs_class =~ /([^:]+)$/;
 }
 
 sub language_column { 'language' }
