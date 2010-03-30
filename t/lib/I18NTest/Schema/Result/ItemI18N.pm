e4954d71ad34f041dcf1f2ca4a36ed7daef822c8
diegok <diego@freekeylabs.com>
2010-03-30 18:20:40 +0200

    I18N manual schema test class modified to the new adhoc model, yay!

--- t/lib/I18NTest/Schema/Result/ItemI18N.pm
+++ t/lib/I18NTest/Schema/Result/ItemI18N.pm
@@ -13,15 +13,13 @@ __PACKAGE__->add_columns(
     { data_type => 'INT', default_value => 0, is_nullable => 0 },
     'language',
     { data_type => 'VARCHAR', default_value => '', is_nullable => 0, size => 2 },
-    'attr',
-    { data_type => 'VARCHAR', is_nullable => 0, size => 32 },
-    'varchar',
+    'string',
     { data_type => 'VARCHAR', is_nullable => 1, size => 255 },
     'text',
     { data_type => 'TEXT', is_nullable => 1 },
 );
 
-__PACKAGE__->set_primary_key("id_item", "language", "attr");
+__PACKAGE__->set_primary_key("id_item", "language");
 __PACKAGE__->belongs_to("id_item", "I18NTest::Schema::Result::Item", { id => "id_item" });
 
 sub testme { 'yay!' }
