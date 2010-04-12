919543a8602c59a374facaa762a508a28f083788
diegok <diego@freekeylabs.com>
2010-04-12 12:33:05 +0200

    Replacing DBICx::TestDatabase with hand maded one that support utf8 on tests

--- t/lib/I18NTest.pm
+++ t/lib/I18NTest.pm
@@ -3,11 +3,19 @@ package # Hide from PAUSE
 
 use strict;
 use warnings;
-use DBICx::TestDatabase;
 
 sub new {
-    my ( $class, $schema_class ) = @_;
-    return DBICx::TestDatabase->new( $schema_class || 'I18NTest::Schema' );
+    my ($class, $schema_class) = @_;
+    $schema_class ||= 'I18NTest::Schema';
+
+    eval "require $schema_class"
+      or die "failed to require $schema_class: $@";
+
+    my $schema = $schema_class->connect("DBI:SQLite::memory:",'','', { sqlite_unicode => 1 })
+      or die "failed to connect to DBI:SQLite::memory: ($schema_class)";
+
+    $schema->deploy;
+    return $schema;
 }
 
 1;
